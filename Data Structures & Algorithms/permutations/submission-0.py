023c379632747e00bfd53c75aaa70b303f70bcc4
Jai Ahuja <jaiahuja187@gmail.com>
2026-06-02 22:57:55 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        op = []
+        def f(L, s):
+            if len(L) == n:
+                op.append(L[:])
+                return
+            for i in nums:
+                if i not in s:
+                    L.append(i)
+                    s.add(i)
+                    f(L, s)
+                    L.pop()
+                    s.remove(i)
+        f([], set())
+        return op

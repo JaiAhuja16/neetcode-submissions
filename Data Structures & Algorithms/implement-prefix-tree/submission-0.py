dfa736b6f12e9f4849ca53c02219b458546b47ee
Jai Ahuja <jaiahuja187@gmail.com>
2026-06-13 11:20:48 +0530

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, char):
+        self.char = char
+        self.adj = {}
+
+class PrefixTree:
+    def __init__(self):
+        self.root = Node('.')
+        self.words = set()
+
+    def insert(self, word: str) -> None:
+        i = 0
+        n = len(word)
+        curr = self.root
+        while i < n and word[i] in curr.adj:
+            curr = curr.adj[word[i]]
+            i += 1
+        while i < n:
+            curr.adj[word[i]] = Node(word[i])
+            curr = curr.adj[word[i]]
+            i += 1
+        self.words.add(word)
+
+    def search(self, word: str) -> bool:
+        return word in self.words
+
+    def startsWith(self, prefix: str) -> bool:
+        i = 0
+        n = len(prefix)
+        curr = self.root
+        while i < n and prefix[i] in curr.adj:
+            curr = curr.adj[prefix[i]]
+            i += 1
+        return i == n
+        
